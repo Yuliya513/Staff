217553496794597700148831d1879a2cb83bd6f5
Yuliya513 <yuliya.senik@gmail.com>
2021-06-02 16:27:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public static void
+        String FirstName;
+        StringLastName;
+        class Staff
+        int workers;
+        int salary;
+        int hours;
+        int salary;
+        int ID;
+        String FirstName=new FirstName[3];
+        String LastName=new LastName[3];
+        StringFirstName="I";
+        String LastName="R";
+        StringFirstName="Io";
+        String LastName="Ro";
+        StringFirstName="IA";
+        String LastName="AR";
+        System.out.println("salary");
+        System.out.println("salary");
+        System.out.println("ID");
+
+    }
+}
